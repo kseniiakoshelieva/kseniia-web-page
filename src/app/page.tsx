4968daf0ede72f601eef365548e579e1cdd95bb6
dima-leveret <dimazayac@gmail.com>
2025-10-06 21:38:21 +0200

    install nodemailer and reactHookForm, implement configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import styles from "./page.module.css";
+import { SendEmailForm } from "./components/SendEmailForm";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>Header</header>
-      <main className={styles.main}>Main</main>
+      <main className={styles.main}>
+        <SendEmailForm />
+      </main>
       <footer className={styles.footer}>Footer</footer>
     </div>
   );
